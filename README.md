5f2a3a0e260077cdf5b9c70380d882a328f3b737
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-05-10 14:48:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Symfony Project
+
+To start project locally 
+```
+symfony server:start -d
+```
+
+To stop project locally 
+```
+symfony server:stop
+```
+
+For more information refer to 
+https://symfony.com/doc/current/setup/symfony_server.html
---
+++ src/Controller/PostController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use App\Services\FileUploader;
+use App\Services\Notification;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/post", name="post.")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    public function index(PostRepository $postRepository)
+    {
+        $posts = $postRepository->findAll();
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @return string
+     */
+    public function create(Request $request, FileUploader $fileUploader, Notification $notification) {
+        // create new post with title
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em =  $this->getDoctrine()->getManager();
+            /** @var UploadedFile $file */
+            $file = $request->files->get('post')['attachment'];
+
+            if($file){
+                $filename = $fileUploader->uploadFile($file);
+                $post->setImage($filename);
+                $em->persist($post);
+                $em->flush();
+            }
+            return $this->redirect($this->generateUrl('post.index'));
+        }
+
+        // return a response
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param Post $post
+     * @return Response
+     */
+    public function show(Post $post) {
+        // create the show post view
+        return $this->render('post/show.html.twig', [
+            'post' => $post
+        ]);
+
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param Post $post
+     * @return Response
+     */
+    public function remove(Post $post) {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        $this->addFlash('success', 'Post was successfully removed');
+
+        return $this->redirect($this->generateUrl('post.index'));
+    }
+}
