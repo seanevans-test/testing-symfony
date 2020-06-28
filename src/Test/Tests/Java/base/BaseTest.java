1f71fe676164353c871a65af4b125f035c6a5fc1
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-28 15:33:41 +0100

    implemented TestNG into the BaseTest, basetest set up

--- src/Test/Tests/Java/base/BaseTest.java
+++ src/Test/Tests/Java/base/BaseTest.java
@@ -1,6 +1,9 @@
 package Java.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import java.HomePage;
@@ -12,7 +15,16 @@ public class BaseTest {
 
     @BeforeTest
     public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        goHome();
 
+        homePage = new HomePage(driver);
     }
 
+    @BeforeMethod
+    public void goHome(){driver.get("http://127.0.0.1:8000");}
+
+    @AfterClass
+    public void tearDown(){driver.quit();}
 }
