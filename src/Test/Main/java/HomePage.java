88dab820e25a74c97d08eba8b919bfa52eccd94d
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-06-28 15:23:31 +0100

    first commit on HomePage

--- src/Test/Main/java/HomePage.java
+++ src/Test/Main/java/HomePage.java
@@ -1,6 +1,14 @@
 package java;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
 
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
     }
+
 }
+
