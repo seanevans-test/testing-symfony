2d835583cea3e1946dfc2edf103002c555e18fc6
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-07-09 21:35:26 +0100

    created page where the login page arrives which allow interaction with posts

---
+++ src/Test/Main/java/PostControllerPage.java
@@ -0,0 +1,10 @@
+package java;
+
+import org.openqa.selenium.WebDriver;
+
+public class PostControllerPage {
+
+    private WebDriver driver;
+
+    public PostControllerPage(WebDriver driver){this.driver = driver;}
+}
