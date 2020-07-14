0e934a8ddefff4657560362cc8b0ae20a3f415dc
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-07-14 20:11:37 +0100

    changed the checkTitle to By.xpath for the title

--- src/Test/Main/java/PostControllerPage.java
+++ src/Test/Main/java/PostControllerPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class PostControllerPage {
 
     private WebDriver driver;
-    private By checkTitle = By.id("title");
+    private By checkTitle = By.xpath("//title");
 
     public PostControllerPage(WebDriver driver){this.driver = driver;}
 
