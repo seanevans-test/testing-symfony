1b1d918f24c2e1b5469bc4ae2dc0e66a4889a3a1
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-07-14 19:43:59 +0100

    first commit on CreatePostPage, added elements to find the dropfile attachment

---
+++ src/Test/Main/java/CreatePostPage.java
@@ -0,0 +1,11 @@
+package java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CreatePostPage {
+
+    private WebDriver driver;
+    private By inputField = By.id("post_attachment");
+    private By uploadButton = By.id("post_token");
+}
