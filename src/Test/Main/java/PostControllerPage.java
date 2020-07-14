3ec6d93b65ca4aab6bc87fd0f7ad2887091bf667
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-07-14 19:27:28 +0100

    added method to check on page title after login

--- src/Test/Main/java/PostControllerPage.java
+++ src/Test/Main/java/PostControllerPage.java
@@ -1,10 +1,14 @@
 package java;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class PostControllerPage {
 
     private WebDriver driver;
+    private By checkTitle = By.id("title");
 
     public PostControllerPage(WebDriver driver){this.driver = driver;}
+
+    public String getTitleText(){return driver.findElement(checkTitle).getText();}
 }
