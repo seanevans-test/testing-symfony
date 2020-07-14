33f462578dba74bd06f55f6f36f225d3e114ece4
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-07-14 20:15:22 +0100

    added the absolute path to the file upload section

--- src/Test/Main/java/CreatePostPage.java
+++ src/Test/Main/java/CreatePostPage.java
@@ -8,4 +8,20 @@ public class CreatePostPage {
     private WebDriver driver;
     private By inputField = By.id("post_attachment");
     private By uploadButton = By.id("post_token");
+    private By uploadedFiles = By.id("uploaded-files");
+
+    public CreatePostPage (WebDriver driver){this.driver = driver;}
+
+    public void clickUploadButton(){
+        driver.findElement(uploadButton).click();
+    }
+
+    public void uploadFile(String absolutePathOfFile){
+        driver.findElement(inputField).sendKeys(absolutePathOfFile);
+        clickUploadButton();
+    }
+
+    public String getUploadedFiles(){
+        return driver.findElement(uploadedFiles).getText();
+    }
 }
