fa4dec46cc2a512f9b7d447a4c1845ab9b734289
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-09-23 21:04:45 +0100

    added TestNG test into LoginTest, testing login credentials

--- src/Test/Tests/loginTests/LoginTests.java
+++ src/Test/Tests/loginTests/LoginTests.java
@@ -1,8 +1,20 @@
 package loginTests;
 
 import Java.base.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.LoginPage;
+import java.PostControllerPage;
 
 public class LoginTests extends BaseTest {
 
-
+    @Test
+    public void testSuccessfulLogin(){
+        LoginPage loginPage = homePage.clickLogin();
+        loginPage.setUsername("admin");
+        loginPage.setPassword("000000");
+        PostControllerPage postControllerPage = loginPage.ClickLoginButton();
+        assertTrue(postControllerPage().findElement(By.linkText("Hello PostController!"), "Invalid Credentials");
+    }
 }
